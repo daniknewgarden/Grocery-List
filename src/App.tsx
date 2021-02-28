cf6be79a2ed0db1d28ab7b33bfdd7ed40089d2ac
Danik <daniknewgarden@gmail.com>
2021-02-28 15:20:52 +0200

    Added Subtitle component
    This is a simple <p> tag.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import MainTitle from "./Components/MainTitle";
 import Title from "./Components/Title";
+import Subtitle from "./Components/Subtitle";
 
 function App() {
   return (
     <div className="App">
       <MainTitle text="Hello world" />
       <Title text="Hello world, but small" />
+      <Subtitle text="Hello world, but subtitle" />
     </div>
   );
 }
