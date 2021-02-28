7de7f96cccc3c6b49f674c8b2cb0503f55e25b1a
Danik <daniknewgarden@gmail.com>
2021-02-28 17:43:20 +0200

    App: added main part css grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,33 @@
 import React from "react";
 import "./App.css";
 import MainTitle from "./Components/MainTitle";
-import Title from "./Components/Title";
-import Subtitle from "./Components/Subtitle";
-import Checkbox from "./Components/Checkbox";
-import Button from "./Components/Button";
-import TextArea from "./Components/Form/Textarea";
 
 function App() {
-  const checkboxTest = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    e.target.checked
-      ? console.log("checkbox checked")
-      : console.log("checkbox unchecked");
-  };
-
-  const buttonTest = (e: React.MouseEvent<HTMLButtonElement>): void => {
-    console.log("click");
-  };
-
-  const textareaTest = (e: React.ChangeEvent<HTMLTextAreaElement>): void => {
-    console.log(e.target.value);
-  };
-
   return (
     <div className="App">
-      <MainTitle text="Hello world" />
-      <Title text="Hello world, but small" />
-      <Subtitle text="Hello world, but subtitle" />
-      <Checkbox text="Checkbox" name="test task" onCheck={checkboxTest} />
-      <Button
-        text="Edit"
-        ariaLabel="click"
-        onClick={buttonTest}
-        transparent
-        danger
-      />
-      <Button text="Add" ariaLabel="click" onClick={buttonTest} big rounded />
-      <TextArea
-        onChange={textareaTest}
-        name="description"
-        title="Description"
-      />
+      <main className="products">
+        <section className="products__part">
+          <MainTitle text="Products" />
+          <ul className="products__list">
+            <li className="products__list-item">item</li>
+            <li className="products__list-item">item</li>
+            <li className="products__list-item">item</li>
+            <li className="products__list-item">item</li>
+            <li className="products__list-item">item</li>
+          </ul>
+        </section>
+        <section className="products__part">
+          <MainTitle text="Completed" />
+          <ul className="products__list">
+            <li className="products__list-item">item</li>
+            <li className="products__list-item">item</li>
+            <li className="products__list-item">item</li>
+            <li className="products__list-item">item</li>
+            <li className="products__list-item">item</li>
+          </ul>
+        </section>
+      </main>
     </div>
   );
 }
-
 export default App;
